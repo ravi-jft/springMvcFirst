dbff2f79330ee5b8b253ac590bd233ef81c71e43
Ravi Kumar <ravikumaraggarwalabc@gmail.com>
2017-04-01 02:25:06 +0530

    intial commit

---
+++ target/tomcat/work/localEngine/localhost/CounterWebApp/org/apache/jsp/index_jsp.java
@@ -0,0 +1,80 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class index_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List _jspx_dependants;
+
+  private javax.el.ExpressionFactory _el_expressionfactory;
+  private org.apache.AnnotationProcessor _jsp_annotationprocessor;
+
+  public Object getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspInit() {
+    _el_expressionfactory = _jspxFactory.getJspApplicationContext(getServletConfig().getServletContext()).getExpressionFactory();
+    _jsp_annotationprocessor = (org.apache.AnnotationProcessor) getServletConfig().getServletContext().getAttribute(org.apache.AnnotationProcessor.class.getName());
+  }
+
+  public void _jspDestroy() {
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+
+    try {
+      response.setContentType("text/html");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+
+      out.write("<html>\n");
+      out.write("<body>\n");
+      out.write("<div class=\"ads-in-post hide_if_width_less_800\">\n");
+      out.write("    <script async src=\"//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\"></script>\n");
+      out.write("    <!-- new 728x90 - After2ndH4 -->\n");
+      out.write("    <ins class=\"adsbygoogle hide_if_width_less_800\"\n");
+      out.write("         style=\"display:inline-block;width:728px;height:90px\"\n");
+      out.write("         data-ad-client=\"ca-pub-2836379775501347\"\n");
+      out.write("         data-ad-slot=\"3642936086\"\n");
+      out.write("         data-ad-region=\"mkyongregion\"></ins>\n");
+      out.write("    <script>\n");
+      out.write("        (adsbygoogle = window.adsbygoogle || []).push({});\n");
+      out.write("    </script>\n");
+      out.write("</div><h2>Hello World!</h2>\n");
+      out.write("</body>\n");
+      out.write("</html>");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          try { out.clearBuffer(); } catch (java.io.IOException e) {}
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
